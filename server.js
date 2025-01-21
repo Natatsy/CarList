6c4f1ca08e04c19e3167705c00f9e5963ebc0904
natatsy <angeltsy2019@icloud.com>
2025-01-20 19:56:46 -0600

    Working onthe links for backend

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 const corsOptions = {
-  origin: " https://Natatsy.github.io",
+  origin: " https://natatsy.github.io",
   methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
   allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
 };
